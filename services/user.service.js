1e034e34c6f3538c30ff3b24c3a8c22544a0dd82
Aleticod <193837@unsaac.edu.pe>
2023-11-15 15:10:45 -0500

    Manage data base connection with pool connection

--- services/user.service.js
+++ services/user.service.js
@@ -1,15 +1,26 @@
 const getConnection = require('./../libs/postgres.js');
+const pool = require('../libs/postgres.pool.js');
 
 class UserService {
-  constructor() {}
+  constructor() {
+    this.pool = pool;
+    this.pool.on('error', (err) => {
+      console.error(err);
+    });
+  }
 
   async create(data) {
     return data;
   }
 
+  // async find() {
+  //   const client = await getConnection();
+  //   const rta = await client.query('SELECT * FROM tasks');
+  //   return rta.rows;
+  // }
+
   async find() {
-    const client = await getConnection();
-    const rta = await client.query('SELECT * FROM tasks');
+    const rta = await this.pool.query('SELECT * FROM tasks');
     return rta.rows;
   }
 }
