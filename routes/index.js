8effd88500b28b1115d3bfce12ebd0aa033e0c7f
Aleticod <193837@unsaac.edu.pe>
2023-11-15 14:42:53 -0500

    Use a users router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routerProducts = require('./products.router.js');
+const routerUsers = require('./users.router.js');
 
 function routerAPI(server) {
   const router = express.Router();
   router.use('/products', routerProducts);
+  router.use('/users', routerUsers);
   server.use('/api/v1', router);
 }
 
