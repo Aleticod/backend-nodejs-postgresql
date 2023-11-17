9dcc8b1a8acf2488e9403f39a56af49a1c6a9101
Aleticod <193837@unsaac.edu.pe>
2023-11-17 15:27:05 -0500

    Create a CRUD router for users

--- routes/users.router.js
+++ routes/users.router.js
@@ -4,10 +4,56 @@ const UserService = require('./../services/user.service');
 const router = express.Router();
 const service = new UserService();
 
+// CREATE
+router.post('/', async (req, res, next) => {
+  try {
+    const body = req.body;
+    const newUser = await service.create(body);
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// READ
 router.get('/', async (req, res, next) => {
   try {
     const users = await service.find();
     res.json(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// READ ONE
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await service.findOne(id);
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// UPDATE
+router.patch('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const updatedUser = await service.update(id, data);
+    res.json(updatedUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await service.delete(id);
+    res.json(deletedUser);
   } catch (error) {
     next(error);
   }
