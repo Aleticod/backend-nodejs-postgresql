a1dda172ce093b0f3bfd5187b1f96132105f9fd9
Aleticod <193837@unsaac.edu.pe>
2023-11-19 23:10:15 -0500

    Add asociation hasOne into user model

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -35,7 +35,9 @@ const UserSchema = {
 
 // Creacion de una clase User
 class User extends Model {
-  static associate() {}
+  static associate(models) {
+    this.hasOne(models.Customer, { as: 'customer', foreignKey: 'userId' });
+  }
 
   static config(sequelize) {
     return {
