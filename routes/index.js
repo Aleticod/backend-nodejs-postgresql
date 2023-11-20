3725daab423e6a088ceb36c8d5fb2a971eecd064
Aleticod <193837@unsaac.edu.pe>
2023-11-19 21:20:28 -0500

    Use a customer model and customer router into index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const routerProducts = require('./products.router.js');
 const routerUsers = require('./users.router.js');
+const routerCustomer = require('./customers.router.js');
 
 function routerAPI(server) {
   const router = express.Router();
   router.use('/products', routerProducts);
   router.use('/users', routerUsers);
+  router.use('/customers', routerCustomer);
   server.use('/api/v1', router);
 }
 
