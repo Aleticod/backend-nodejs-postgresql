872efce090ebeaae2f647b5fa4ef72e21f35d629
Aleticod <193837@unsaac.edu.pe>
2023-11-21 12:48:26 -0500

    Add category and product ORM model

---
+++ db/models/category.model.js
@@ -0,0 +1,45 @@
+const { Model, Sequelize, DataTypes } = require('sequelize');
+
+const CATEGORY_TABLE = 'categories';
+
+const CategorySchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  name: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'created_at',
+    defaultValue: Sequelize.NOW,
+  },
+};
+
+// create a model class
+class Category extends Model {
+  static associate(models) {
+    this.hasMany(models.Product, { as: 'product', foreignKey: 'categoryId' });
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: CATEGORY_TABLE,
+      modelName: 'Category',
+      timestamps: false,
+    };
+  }
+}
+
+module.exports = { CategorySchema, Category, CATEGORY_TABLE };
---
+++ db/models/product.model.js
@@ -0,0 +1,68 @@
+const { Model, Sequelize, DataTypes } = require('sequelize');
+
+const { CATEGORY_TABLE } = require('./category.model');
+
+const PRODUCT_TABLE = 'products';
+
+const ProductSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'created_at',
+    defaultValue: Sequelize.NOW,
+  },
+  categoryId: {
+    field: 'category_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    unique: false,
+    references: {
+      model: CATEGORY_TABLE,
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  },
+};
+
+// create a model class
+class Product extends Model {
+  static associate(models) {
+    this.belongsTo(models.Category, {
+      as: 'category',
+    });
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: PRODUCT_TABLE,
+      modelName: 'Product',
+      timestamps: false,
+    };
+  }
+}
+
+module.exports = { PRODUCT_TABLE, ProductSchema, Product };
