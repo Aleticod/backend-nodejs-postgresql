76907430e805bc61c89f381ff8fd1be4a4f86f0c
Aleticod <193837@unsaac.edu.pe>
2023-11-21 12:51:05 -0500

    Add category and product router into index routers

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const routerProducts = require('./products.router.js');
 const routerUsers = require('./users.router.js');
 const routerCustomer = require('./customers.router.js');
+const routerCategory = require('./category.router.js');
 
 function routerAPI(server) {
   const router = express.Router();
   router.use('/products', routerProducts);
   router.use('/users', routerUsers);
   router.use('/customers', routerCustomer);
+  router.use('/categories', routerCategory);
   server.use('/api/v1', router);
 }
 
