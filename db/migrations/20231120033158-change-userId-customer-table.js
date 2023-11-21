9257c3dbe7da198e0cbd6719e93ec561621ab58c
Aleticod <193837@unsaac.edu.pe>
2023-11-20 19:17:50 -0500

    Add other form to add constrains

--- db/migrations/20231120033158-change-userId-customer-table.js
+++ db/migrations/20231120033158-change-userId-customer-table.js
@@ -19,3 +19,19 @@ module.exports = {
 
   async down(queryInterface) {},
 };
+
+
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up(queryInterface) {
+//     await queryInterface.addConstraint('costumers', {
+//       fields: ['user_id'],
+//       type: 'unique',
+//       name: 'unique_user_id',
+//     });
+//   },
+
+//   async down(queryInterface) {
+//     await queryInterface.removeConstraint('costumers', 'unique_user_id');
+//   },
+// };
